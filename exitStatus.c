dad356e1663c0fcd2d832be279b6c7cca1aed3bc
TomasNagy <tomi.nagy@outlook.com>
2023-05-12 10:45:35 +0200

    Add exitStatus.c

---
+++ exitStatus.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+	if (argc != 2)	
+	{
+		printf("Missing argument!\n");
+	}
+	else
+	{
+		printf("Hello, %s %s!\n", argv[1] argv[2]);
+	}
+}
