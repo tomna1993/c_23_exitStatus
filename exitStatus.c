68c31fe68168015d2c5c312d810b7d7ec36b6972
TomasNagy <tomi.nagy@outlook.com>
2023-05-12 12:45:54 +0200

    Correct comments

--- exitStatus.c
+++ exitStatus.c
@@ -6,11 +6,15 @@ int main(int argc, string argv[])
 	// We check if argc has user input or not. 
 	// argc = 1 if we run our program without any arguments int the terminal -> ./build/exitStatus.exe
 	// argv[0] = "./build/exitStatus.exe"
-	// argv[1] = "null"
+	
 	// In our program argc = 2 because we add one argument in CLI when we run the code -> ./build/exitStatus.exe Name 
 	// argv[0] = "./build/exitStatus.exe"
 	// argv[1] = "Name"
-	// argv[2] = "null"
+
+	// We can't use argv[2] or check array elements with higher index then argc-1 because 
+	// we step out of the argv array boundary. In that area of memory can be any value placed by other programs
+	// so it is dangerous to modify or eaven read those elements.
+
 	if (argc != 2)	
 	{
 		printf("Missing argument!\n");
